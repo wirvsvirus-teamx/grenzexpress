e70c5c5d97ddcef50f288c5b08782544049e40e1
Jonasdoubleyou <jonaswilms2000@gmail.com>
2020-03-22 12:31:50 +0100

    final version from Simon

--- frontend/src/components/signature/Signature.tsx
+++ frontend/src/components/signature/Signature.tsx
@@ -1,5 +1,11 @@
-import React, { FunctionComponent, useEffect, useRef } from 'react';
+/* eslint-disable no-unused-expressions */
+/* eslint-disable consistent-return */
+import { Button } from '@material-ui/core';
+import React, {
+  useEffect, useRef, useState,
+} from 'react';
 
+import { IQuestionProps } from '../../types';
 import style from './Signature.module.scss';
 
 function getMousePos(canvas: HTMLCanvasElement, evt: MouseEvent | Touch) {
@@ -9,8 +15,21 @@ function getMousePos(canvas: HTMLCanvasElement, evt: MouseEvent | Touch) {
     y: evt.clientY - rect.top,
   };
 }
-export const Signature: FunctionComponent = () => {
+export const Signature = ({
+  question, setAnswer, answer, removeAnswer,
+}: IQuestionProps<'signature'>) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [pointCount, setPointCount] = useState(0);
+  const canSign = pointCount >= 1 && !answer;
+  function sign() {
+    if (!canvasRef.current) return;
+    const signature = canvasRef.current.toDataURL();
+    setAnswer({
+      id: question.id,
+      type: 'signature',
+      signature,
+    });
+  }
   useEffect(() => {
     let started = false;
     if (!canvasRef.current) {
@@ -21,12 +40,22 @@ export const Signature: FunctionComponent = () => {
       return;
     }
     const context = canvas.getContext('2d');
-    if (context) {
+    if (context && !answer) {
+      const touchStart = (evt: TouchEvent) => {
+        startSignature(evt.touches[0]);
+      };
+      const touchMove = (evt: TouchEvent) => {
+        updateSignature(evt.changedTouches[0]);
+      };
+      const touchEnd = (evt: TouchEvent) => {
+        endSignature(evt.targetTouches[0]);
+      };
       const startSignature = (evt: MouseEvent | Touch) => {
         const mousePos = getMousePos(canvas, evt);
         context.beginPath();
         context.moveTo(mousePos.x, mousePos.y);
         started = true;
+        setPointCount((p) => p + 1);
       };
       const updateSignature = (evt: MouseEvent | Touch) => {
         if (started) {
@@ -46,23 +75,50 @@ export const Signature: FunctionComponent = () => {
           started = false;
         }
       };
-      canvas.addEventListener('touchstart', (evt) => {
-        startSignature(evt.touches[0]);
-      });
-      canvas.addEventListener('touchmove', (evt) => {
-        updateSignature(evt.changedTouches[0]);
-      });
-      canvas.addEventListener('touchend', (evt) => {
-        endSignature(evt.targetTouches[0]);
-      });
+      canvas.addEventListener('touchstart', touchStart);
+      canvas.addEventListener('touchmove', touchMove);
+      canvas.addEventListener('touchend', touchEnd);
       canvas.addEventListener('mousedown', startSignature);
-      canvas.addEventListener('mousemove', updateSignature);
       canvas.addEventListener('mouseup', endSignature);
+      canvas.addEventListener('mousemove', updateSignature);
+      return () => {
+        canvas.removeEventListener('mousedown', startSignature);
+        canvas.removeEventListener('mouseup', endSignature);
+        canvas.removeEventListener('mousemove', updateSignature);
+        canvas.removeEventListener('touchstart', touchStart);
+        canvas.removeEventListener('touchmove', touchMove);
+        canvas.removeEventListener('touchend', touchEnd);
+      };
     }
-  });
+  }, [answer]);
+  // Restore & clear images
+  React.useEffect(() => {
+    if (canvasRef.current?.getContext('2d')) {
+      const context = canvasRef.current?.getContext('2d');
+      if (answer) {
+        const image = new Image();
+        image.onload = () => {
+          context?.drawImage(image, 0, 0);
+        };
+        image.src = `data:image/png;base64${answer.signature}`;
+      } else {
+        context?.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+      }
+    }
+  }, [answer, canvasRef]);
   return (
-    <canvas ref={canvasRef} className={style.border} height="200" id="canvas" width="300">
-      <p>Please activate JavaScript.</p>
-    </canvas>
+    <>
+      <canvas ref={canvasRef} className={style.border} height="200" id="canvas" width="300">
+        <p>Please activate JavaScript.</p>
+      </canvas>
+      <br />
+      <br />
+      <Button color="secondary" disabled={!answer} variant="contained" onClick={() => { setPointCount(0); removeAnswer(question.id); }}>
+        Zurücksetzen
+      </Button>
+      <Button color="primary" disabled={!canSign} variant="contained" onClick={sign}>
+        Signieren
+      </Button>
+    </>
   );
 };
