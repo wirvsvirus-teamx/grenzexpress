7651b3c6057bf8e42d46bced9943dccbb089f894
Simon Petrik <simon.petrik@protonmail.ch>
2020-03-22 13:04:18 +0100

    Fixed input text bug (last char not deletable)

--- frontend/src/components/question-text-input/TextInput.tsx
+++ frontend/src/components/question-text-input/TextInput.tsx
@@ -3,9 +3,12 @@ import React from 'react';
 
 import { IQuestionProps } from '../../types';
 
-export const TextInput = ({ question, answer, setAnswer }: IQuestionProps<'text-input'>) => {
+export const TextInput = ({
+  question, answer, setAnswer, removeAnswer
+}: IQuestionProps<'text-input'>) => {
   function setValue(value: string) {
     if (value) setAnswer({ id: question.id, type: 'text-input', value });
+    else removeAnswer(question.id);
   }
 
   return (
