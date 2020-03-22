47227aec6de565b6fefc3d322bdf96be271e2a38
Jonasdoubleyou <jonaswilms2000@gmail.com>
2020-03-22 02:21:28 +0100

    form signature, form states

--- frontend/src/components/finished-form/FinishedForm.tsx
+++ frontend/src/components/finished-form/FinishedForm.tsx
@@ -14,6 +14,9 @@ const useStyles = makeStyles(() => createStyles({
   green: {
     color: '#00ff00',
   },
+  yellow: {
+    color: '#f9b000',
+  },
 }));
 
 export const FinishedForm = ({ formAnswer }: { formAnswer: IFormAnswer }) => {
@@ -21,6 +24,14 @@ export const FinishedForm = ({ formAnswer }: { formAnswer: IFormAnswer }) => {
   const classes = useStyles();
 
   if (!form) throw new Error(`Form with id ${formAnswer.id} not found`);
+
+  const { state, message } = form?.validate((answerID: any) => formAnswer.answers.find((it) => it.id === answerID) as any);
+
+  const color = ({
+    valid: 'green',
+    invalid: 'red',
+    unknown: 'yellow',
+  } as const)[state];
 
   return (
     <Grid item sm={4} xs={12}>
@@ -30,11 +41,17 @@ export const FinishedForm = ({ formAnswer }: { formAnswer: IFormAnswer }) => {
             {form.title}
           </Typography>
         </CardContent>
-        <CardActions className={classes.red}>
+        <CardActions className={classes[color]}>
           <FiberManualRecordIcon />
           <p>
-            Einreise nicht genehmigt
+            {message}
           </p>
+          {state === 'unknown' && (
+          <p>
+            Die Entscheidung trifft ein Kollege vor Ort.
+          </p>
+          )}
+
         </CardActions>
       </Card>
     </Grid>
